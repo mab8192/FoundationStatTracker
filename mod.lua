3e0d296123988fd7f18d8f207eaa06c74b821609
mab8192 <mab8192@gmail.com>
2026-03-14 19:04:31 -0600

    production logging is working

--- mod.lua
+++ mod.lua
@@ -1,28 +1,72 @@
 local mod = foundation.createMod()
 
--- Declare a new component
 local COMP_STAT_CONTROLLER = {
     TypeName = "COMP_STAT_CONTROLLER",
     ParentType = "COMPONENT",
     Properties = {}
 }
 
-mod:log("Starting up!")
+mod:log("Resource Stats Tracker starting up!")
 
--- When that component gets enabled, start doing stuff
+local dailyStats = {}
+local weeklyStats = {}
+local monthlyStats = {}
+
+function COMP_STAT_CONTROLLER:registerProductionListeners()
+    local workplaceManager = self:getLevel():getComponentManager("COMP_WORKPLACE")
+    if not workplaceManager then
+        mod:log("Workplace manager does not exist.")
+        return
+    end
+
+    local comps = workplaceManager:getAllComponent()
+    mod:log("Found " .. tostring(#comps) .. " workplaces.")
+
+    comps:forEach(function(comp)
+        event.register(self, comp.ON_WORKPLACE_PRODUCED, function(res)
+            local collection = res.Collection
+            collection:forEach(function(rqpair)
+                ---@type RESOURCE
+                local resource = rqpair.Resource
+                ---@type integer
+                local quant = rqpair.Quantity
+                dailyStats[resource.ResourceName] = dailyStats[resource.ResourceName] or 0
+                dailyStats[resource.ResourceName] = dailyStats[resource.ResourceName] + quant
+                weeklyStats[resource.ResourceName] = weeklyStats[resource.ResourceName] or 0
+                weeklyStats[resource.ResourceName] = weeklyStats[resource.ResourceName] + quant
+                monthlyStats[resource.ResourceName] = monthlyStats[resource.ResourceName] or 0
+                monthlyStats[resource.ResourceName] = monthlyStats[resource.ResourceName] + quant
+            end)
+        end)
+    end)
+end
+
 function COMP_STAT_CONTROLLER:onEnabled()
+    mod:log("COMP_STAT_CONTROLLER ENABLED!")
 
-    mod:log("COMPONENT ENABLED!")
+    self:registerProductionListeners()
 
+    ---@type COMP_MAIN_GAME_LOOP
     local compMainGameLoop = self:getLevel():find("COMP_MAIN_GAME_LOOP")
+
     event.register(self, compMainGameLoop.ON_NEW_DAY, function()
-        mod:log("NEW DAY!")
+        mod:log("New Day!")
+        mod:log("Daily Stats: \n" .. tostring(dailyStats))
+        dailyStats = {} -- reset stats
     end)
 
-end -- component init end
+    event.register(self, compMainGameLoop.ON_NEW_WEEK, function()
+        mod:log("New Week!")
+        mod:log("Weekly Stats: \n" .. tostring(weeklyStats))
+        weeklyStats = {} -- reset stats
+    end)
 
--- Register the custom class
+    event.register(self, compMainGameLoop.ON_NEW_MONTH, function()
+        mod:log("New Month!")
+        mod:log("Monthly Stats: \n" .. tostring(monthlyStats))
+        monthlyStats = {} -- reset stats
+    end)
+end
+
 mod:registerClass(COMP_STAT_CONTROLLER)
-
--- Attach class to the prefab manager which means it will be present in the game from the beginning
-mod:registerPrefabComponent("PREFAB_MANAGER", { DataType = "COMP_STAT_CONTROLLER" } )
+mod:registerPrefabComponent("PREFAB_MANAGER", { DataType = "COMP_STAT_CONTROLLER" })
